4cab49eda089b605800b8ef653943bfb4272232d
viktorstrate <viktorstrate@gmail.com>
2020-04-20 18:44:58 +0200

    Further work on buttons + add link to logo

--- components/Button.js
+++ components/Button.js
@@ -8,18 +8,36 @@ const ButtonStyled = styled.button`
   font-family: 'Delius', sans-serif;
   font-size: 24px;
   background-color: #ffd222;
-  box-shadow: -4px 4px 0 #dfb50f;
+  /* box-shadow: -4px 4px 0 #dfb50f; */
   color: white;
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    background-color: #dfb50f;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    transform: translate(-22px, -4px) rotate(1.5deg);
+  }
 
   &:hover {
-    box-shadow: -6px 6px 0 #e4be2a;
+    &::before {
+      transform: translate(-20px, -6px) rotate(-1deg);
+    }
+
     cursor: pointer;
     background-color: #ffdd56;
   }
 
   &:disabled {
+    &::before {
+      transform: translate(-20px, -9px) rotate(1.5deg);
+      background-color: #ccc;
+    }
+
     cursor: default;
-    box-shadow: -2px 2px 0 #ccc;
     background-color: #eee;
   }
 `
--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import Link from 'next/link'
 
 const LogoImg = styled.img`
   width: 400px;
   display: block;
+  cursor: pointer;
 
   ${({ centered }) =>
     centered &&
@@ -14,6 +16,12 @@ const LogoImg = styled.img`
 
 export default function Logo({ centered }) {
   return (
-    <LogoImg alt="Babble doodle logo" centered={centered} src="/img/logo.svg" />
+    <Link href="/">
+      <LogoImg
+        alt="Babble doodle logo"
+        centered={centered}
+        src="/img/logo.svg"
+      />
+    </Link>
   )
 }
